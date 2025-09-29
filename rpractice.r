d6751cf7a4cf46ece664fcf70c7e8142b1204e3e
Johanna Bayer <hannabayer.89@gmail.com>
2025-09-29 20:41:06 +0200

    added new variable

--- rpractice.r
+++ rpractice.r
@@ -8,3 +8,4 @@ library(gtsummary)
 library(readxl)
 library(plotly)
 mm <- read.csv("/Users/axg999/Library/CloudStorage/Box-Box/Amanda\'s\ Life/ReproRehab/Pod\ 5\ 2025/Practice/tulip.csv")
+new_vairable <- mm$X1 + mm$X2 + mm$X3 + mm$X4 + mm$X5
