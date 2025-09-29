1cf5d9fb6bbf3cbae61fc06aad1467f6d78c2d94
Johanna Bayer <hannabayer.89@gmail.com>
2025-09-29 20:46:52 +0200

    Add comment to new variable assignment
    Added a comment to the new variable assignment.

--- rpractice.r
+++ rpractice.r
@@ -9,3 +9,4 @@ library(readxl)
 library(plotly)
 mm <- read.csv("/Users/axg999/Library/CloudStorage/Box-Box/Amanda\'s\ Life/ReproRehab/Pod\ 5\ 2025/Practice/tulip.csv")
 new_vairable <- mm$X1 + mm$X2 + mm$X3 + mm$X4 + mm$X5
+# make a comment
